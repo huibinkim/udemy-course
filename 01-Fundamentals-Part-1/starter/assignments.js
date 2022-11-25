563eee43475cbd1aca589396c03e34309b1bae2f
abc00127@naver.com <abc00127@naver.com>
2022-11-25 11:58:00 +0900

    20221125 study

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,8 @@
+
+let country = "Korea";
+let continent = "Asia";
+let population = "5174 millions";
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,22 @@
+// let js = 'amazing';
+// if(js === 'amazing') alert('javascript is jun');
+
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
+
+console.log("jonas");
+
+let firstName = "bob";
+let first = 'jonas';
+let firstNamePerson
+let first_name_person
+console.log(firstName);
+
+let PI = 3.141;
+
+let myFirstJob = 'programmer';
+let myCurrentJob = "teacher";
+
+// 위의 변수 이름이 더 직관적임. 
+let job1 = 'programmer';
+let job2 = 'teacher';
