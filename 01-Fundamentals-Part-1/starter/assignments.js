d15acbdeb1b0e2036a98861cedb3155823356ebf
abc00127@naver.com <abc00127@naver.com>
2022-11-25 15:58:26 +0900

    add lecture

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,8 +1,34 @@
+// lecture : values and variables
+const country = "Korea";
+const continent = "Asia";
+let population = 5174;
 
-let country = "Korea";
-let continent = "Asia";
-let population = "5174 millions";
+
+// lecture : data types
+const isIsland = true;
+const language = 'korean';
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+// lecture: var, let, const
+population = 5715
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// lecture : Basic operators
+halfPopulation = population / 2;
+population++;
+console.log(population);
+
+let finLand = 6000;
+console.log(population > finLand);
+let averagePopulation = 3300;
+console.log(population < averagePopulation);
+
+const description = country + ' is in ' + continent +', and its ' + population + ' million people speak ' + language;
+console.log(description);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,6 @@
 // let js = 'amazing';
 // if(js === 'amazing') alert('javascript is jun');
-
+/*
 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
 
@@ -12,6 +12,7 @@ let firstNamePerson
 let first_name_person
 console.log(firstName);
 
+// variable name conventions 11~12강의
 let PI = 3.141;
 
 let myFirstJob = 'programmer';
@@ -20,3 +21,67 @@ let myCurrentJob = "teacher";
 // 위의 변수 이름이 더 직관적임. 
 let job1 = 'programmer';
 let job2 = 'teacher';
+
+
+true;
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'jo');
+
+javascriptIsFun = 'yes!';
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+
+year = 199;
+console.log(year);
+
+console.log(typeof null); 
+
+
+// 13강의
+let age = 30;
+age = 31;
+console.log(age); 
+
+const birthYear = 1991;
+
+var job = 'programmer';
+job = 'teacher';
+
+*/
+
+// 14. basic operation
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = 'Jonas';
+const lastName = 'Huibin';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5;
+x += 10; //x = x + 10;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // > , <, >= , <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2019); 
