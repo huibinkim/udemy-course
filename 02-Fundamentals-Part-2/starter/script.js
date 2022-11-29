77d97f6d9f788dc71b55acf7b01f53d0d7cb7622
abc00127@naver.com <abc00127@naver.com>
2022-11-29 15:58:52 +0900

    section3 lec 32

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+// lec 32
+'use strict';
+
+let hasDriversLicense = false;
+const  passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log('i can drive');
+
+// const interFace = 'Audio';
+// const private = 43;
+// const if = 23;
