41800d1a725f6ca479e9f7dc8dc74c04a6b0e5f4
abc00127@naver.com <abc00127@naver.com>
2022-12-02 17:43:29 +0900

    lec 44

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -246,8 +246,35 @@ console.log(`${jonas['firstName']} has ${jonas.friend.length}, and his best frie
 const jonas = {
   firstName : 'Jonas',
   lastName : 'Chris',
-  birthYear: 2037 - 1991, 
+  birthYear: 1991, 
   job: 'teacher',
   friend: ['Michael', 'Peter', 'Steven'],
-  hasDriversLicense: true
+  hasDriversLicense: true,
+
+  // calcAge: function(birthYear){
+  //   return 2037 - birthYear;
+  // }
+
+  // calcAge: function(){
+  //   console.log(this);
+  //   return 2037 - this.birthYear;
+  // }
+
+  calcAge: function(){
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function(){
+    return `${this.firstName} is a ${this.calcAge()} and he has ${this.hasDriversLicense ? 'a' : 'no'}`
+  }
 };
+
+console.log(jonas.calcAge());
+console.log(jonas.age);
+// console.log(jonas['calcAge'](1993));
+
+// Challenge
+// "Jonas is a 46 year old teacher, and he has a driver's license"
+
+console.log(jonas.getSummary());
