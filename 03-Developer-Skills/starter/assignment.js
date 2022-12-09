d12587623ad0cd0605585d18c9d6d0d3834d08e7
abc00127@naver.com <abc00127@naver.com>
2022-12-09 13:51:59 +0900

    20221209

--- 03-Developer-Skills/starter/assignment.js
+++ 03-Developer-Skills/starter/assignment.js
@@ -30,7 +30,7 @@ const data2 = [12, 5, -5, 0, 4];
 //   for (let i = 0; i < arr.length; i++) {
 //     const temp = arr[i];
 //     console.log(` ${temp} in ${i + 1}days`);
-//   }
+//   }`
 // };
 
 // const realTemp = printForecast(data1);
