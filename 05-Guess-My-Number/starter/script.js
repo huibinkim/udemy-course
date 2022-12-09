a0f61a2fe70f7cc58cb8399063d2e5119e0b15bd
abc00127@naver.com <abc00127@naver.com>
2022-12-09 15:01:37 +0900

    game project

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+/*
+// lec 72. selecting and manipulating elements.
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!🎀';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 80;
+console.log(document.querySelector('.guess').value);
+*/
+
+// lec 73. handling click event
+const x = function () {
+  console.log(23);
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
+});
