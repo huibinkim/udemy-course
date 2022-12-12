f5d70d9ec797b3dc4527bbe60db032974b1f2b18
abc00127@naver.com <abc00127@naver.com>
2022-12-12 14:19:28 +0900

    lec 74

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -13,17 +13,39 @@ document.querySelector('.guess').value = 80;
 console.log(document.querySelector('.guess').value);
 */
 
-// lec 73. handling click event
-const x = function () {
-  console.log(23);
-};
+// lec 73. handling click event //
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!🎀';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!🎈';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🎃 you lost the game ';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!🧨';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🎃 you lost the game ';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
+
+// lec 74. implementing the game logic
