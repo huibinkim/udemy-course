1eec310afacc4eff59d49a5f3f8828cb2e76bd68
abc00127@naver.com <abc00127@naver.com>
2022-12-13 16:14:13 +0900

    project 3 pig game finish

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -14,38 +14,103 @@ const newBtn = document.querySelector('.btn--new');
 const rollBtn = document.querySelector('.btn--roll');
 const holdBtn = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playing;
+
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
 
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 // Click roll dice functionality
 rollBtn.addEventListener('click', function () {
-  // 1. Generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+  if (playing === true) {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
 
-  // 2. Display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  // 3. Check for rolled 1 : if true,
-  if (dice !== 1) {
-    // Add dice to current score
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-    //current0El.textContent = currentScore; //Change later.
-  } else {
-    // Switch to next player : continue
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    // 3. Check for rolled 1 : if true,
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+      //current0El.textContent = currentScore; //Change later.
+    } else {
+      // Switch to next player : continue
+      switchPlayer();
+    }
   }
 });
+
+holdBtn.addEventListener('click', function () {
+  if (playing) {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] + currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 100) {
+      // Finish the game
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
+  // Switch to the next player
+});
+
+newBtn.addEventListener('click', init);
+// 1. current , score is go back in 0
+// 내 코드
+// currentScore = 0;
+// document.getElementById(`score--${activePlayer}`).textContent = currentScore;
+// document.getElementById(`current--${activePlayer}`).textContent =
+//   currentScore;
+
+// document
+//   .querySelector(`.player--${activePlayer}`)
+//   .classList.remove('player--winner');
+// document
+//   .querySelector(`.player--${activePlayer}`)
+//   .classList.add('player--active');
+// 2. remove player--winner
+
+// 3. game is playing
+// if ((scores[activePlayer] = 0)) {
+//   playing = true;
+// }
