1a32b32c092bd0b1106c5e6a169eab62b6a4937f
abc00127@naver.com <abc00127@naver.com>
2022-12-13 12:32:53 +0900

    project 3 start pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,51 @@
 'use strict';
+
+// Selecting elements score
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const diceEl = document.querySelector('.dice');
+const newBtn = document.querySelector('.btn--new');
+const rollBtn = document.querySelector('.btn--roll');
+const holdBtn = document.querySelector('.btn--hold');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+// Click roll dice functionality
+rollBtn.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1 : if true,
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    //current0El.textContent = currentScore; //Change later.
+  } else {
+    // Switch to next player : continue
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+});
